feb956b99e9891ca5f583c71a901380167aaf1f6
AsilbekZarifovpy <asilbekzarifovpy@gmail.com>
2025-03-18 08:55:56 +0500

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -308,12 +308,48 @@ Created on Sun Mar 16 20:50:05 2025
 
 # print(summa(1,2,35))
 
-def talaba_info(ism, familiya, **kwargs):
-    kwargs['ism']=ism
-    kwargs['familiya']=familiya
-    return kwargs
+# def talaba_info(ism, familiya, **kwargs):
+#     kwargs['ism']=ism
+#     kwargs['familiya']=familiya
+#     return kwargs
 
-talaba = talaba_info('olim','olimov',tyil=1995,fakultet='IT',yonalish='AT')
+# talaba = talaba_info('olim','olimov',tyil=1995,fakultet='IT',yonalish='AT')
 
+
+def avto_info(kompaniya, model, rangi, korobka, yili, narhi=None):
+    """Avtomobil haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
+    avto = {'kompaniya':kompaniya,
+            'model':model,
+            'rang':rangi,
+            'korobka':korobka,
+            'yil':yili,
+            'narh':narhi}
+    return avto
+
+def avto_kirit():
+    """Foydalanuvchiga avto_info funksiyasi yordamida bir nechta avtolar haqida ma'lumotlarni bitta ro'yxatga joylash imkonini beruvchi funksiya"""
+    avtolar=[] # salondagi avtolar uchun bo'sh ro'yxat
+    while True:
+        print("\nQuyidagi ma'lumotlarni kiriting",end='')
+        kompaniya=input("Ishlab chiqaruvchi: ")
+        model=input("Modeli: ")
+        rangi=input("Rangi: ")
+        korobka=input("Korobka: ")
+        yili=input("Ishlab chiqarilgan yili: ")
+        narhi=input("Narhi: ")    
+        #Foydalanuvchi kiritdan ma'lumotlardan avto_info yordamida 
+        #lug'at shakllantirib, har bir lug'atni ro'yxatga qo'shamiz:
+        avtolar.append(avto_info(kompaniya, model, rangi, korobka, yili, narhi))    
+        # Yana avto qo'shish-qo'shmaslikni so'raymiz
+        javob = input("Yana avto qo'shasizmi? (yes/no): ")
+        if javob=='no':
+            break
+    return avtolar
+
+def info_print(avto_info):
+    """Avtomobillar haqida ma'lumotlar saqlangan lug'atni konsolga chiqaruvchi funksiya"""
+    print(f"{avto_info['rang'].title()} {avto_info['kompaniya'].upper()} "
+          f"{avto_info['model'].upper()}, {avto_info['korobka']} korobka, "
+          f"{avto_info['yil']}-yil, {avto_info['narh']}$")
 
     
